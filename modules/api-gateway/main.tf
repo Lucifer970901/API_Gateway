9aa9c3d8d1f681a8777a420805d4210cf6bd75ac
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-28 14:39:22 +0530

    Update main.tf

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -30,13 +30,13 @@ locals {
 
 
 resource "oci_apigateway_api" "this" {
-    for_each = fileset("${path.module}/openapi", "*.yaml")
+    for_each = fileset("${path.module}../../data/openapi.yaml", "*.yaml")
     compartment_id = var.compartment_ids[each.value.compartment_name]
     #content = file("${path.module}/openapi/${each.value}")
     #display_name = yamldecode(file("${path.module}/openapi/${each.value}")).info.title  
     #compartment_id = var.compartment_id
-    content =  file("./data/openapi.yaml")
-    display_name = yamldecode(file("./data/openapi.yaml")).info.title  
+    content =  data.local_file.api_description_file.content
+    display_name = local.api_description.info.title
 }
   locals {
   backend_types = {
